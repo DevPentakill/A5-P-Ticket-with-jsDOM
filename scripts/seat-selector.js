b9e38c80af9750f273a379bf83446c733eda3ded
DevPentakill <fmahdi.bd@gmail.com>
2024-02-22 03:38:57 +0600

    total and grand price calculation added

--- scripts/seat-selector.js
+++ scripts/seat-selector.js
@@ -1,3 +1,5 @@
+const category = "Economy";
+const priceTckt = 550;
 const seats = document.getElementsByClassName("bus-seat");
 // console.log(seats);
 //it shows me html collection
@@ -8,16 +10,29 @@ seat.addEventListener("click", function(event){
 const seatNum= event.target.textContent;
 // console.log(typeof seatNum);
 //give me the seat number as a string
-const category = "Economy";
-const priceTckt = 550;
+
+
+
+event.target.setAttribute("disabled", false);
+
+if (
+  getValueById("seats-added")>=4) {
+  alert("You have reached seat booking Limit");
+  return;
+}
+
+event.target.style.backgroundColor = 'rgb(29, 209, 0)';
+event.target.style.color = "white";
 
 const tableHolder = document.getElementById("selected-seat-list");
-
 const newRow= createTableRows(seatNum,category,priceTckt);
-
 tableHolder.innerHTML += newRow;
 updateSeatsLeft();
 updateSeatsAdded();
+updateTotalCost(priceTckt);
+updateGrandTotal();
+
+
 
 });
 
@@ -48,4 +63,35 @@ function getValueById(id) {
     const convertDefaultSeatCount = parseInt(defaultSeatCount);
     document.getElementById("seats-added").innerText = convertDefaultSeatCount + 1;
   }
+
+  function updateTotalCost(price) {
+    const previousTotal = document.getElementById("total-price").innerText;
+    const convertedTotal = parseInt(previousTotal);
+    const convertedPrice = parseInt(price);
+    document.getElementById("total-price").innerText = convertedTotal + convertedPrice;
+  }
   
+  function updateGrandTotal(control) {
+    const previousTotal = document.getElementById("total-price").innerText;
+    const convertedTotal = parseInt(previousTotal);
+    const couponCode = document.getElementById("coupon-code").value;
+    
+    if (control) {
+      if (couponCode == "Couple 20") {
+        const discount = convertedTotal * 0.2;
+        document.getElementById("grand-price").innerText =
+          convertedTotal - discount;
+        
+
+      }else if(couponCode == "NEW15") {
+        const discount = convertedTotal * 0.15;
+        document.getElementById("grand-price").innerText =
+          convertedTotal - discount;
+      } else {
+        alert("Invalid Coupon Code");
+        return;
+      }
+    } else {
+      document.getElementById("grand-price").innerText = convertedTotal;
+    }
+  }
